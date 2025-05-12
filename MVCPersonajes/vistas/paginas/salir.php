904b455ebf74ba43127400b570e85f82a035e94d
Sergio Quetzal Jimenez Tinoco <sergio.q.jimenez.t@gmail.com>
2025-05-12 16:09:40 -0600

    Actualizamos salir.php
    Se actualiza un poco mas la infromacion del desarrollador dentro de la seccion de About me

--- MVCPersonajes/vistas/paginas/salir.php
+++ MVCPersonajes/vistas/paginas/salir.php
@@ -3,3 +3,5 @@
 <h2>Hola, soy Sergio Quetzal, estudiante de Ingeniería en Sistemas Computacionales con especialidad en desarrollo web. Me apasiona la creación de interfaces intuitivas, la gestión de bases de datos relacionales y el desarrollo de soluciones tecnológicas innovadoras. Siempre estoy abierto a nuevos aprendizajes y retos.</h2>
 <br>
 <h3>soy estudiante de la carrera de Sistemas computacionales con especialidad en paginas web, me gusta mucho inovar y trabajar con nuevas tecnologias, adaptable a los cambios y a los entornos</h3>
+<br>
+<h4>Me gusta meter algunos Easter Eggs en mis codigos entonces, talvez solo sean caritas, dibujos, imagenes o cositas asi para hacer menos aburrida la experiencia del usuario 0^0 '<(i see u)'</h4>
