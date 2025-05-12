2c13d3e1907e80dd858854d427235f4aeddfc6a6
Sergio Quetzal Jimenez Tinoco <sergio.q.jimenez.t@gmail.com>
2025-05-12 16:02:38 -0600

    Actualizamos la vista de salir
    Se actualizo el contenido de esta vista, pues se agrego mas informacion del desarrollador

--- MVCPersonajes/vistas/paginas/salir.php
+++ MVCPersonajes/vistas/paginas/salir.php
@@ -1,2 +1,5 @@
 <h1>Un gusto.<br> Soy Sergio y soy un desarrollador Jr en paginas web, interesado en la creacion de paginas web, manejo buenas practicas en la creacion de bases de datos, soy una persona muy interesada en la cultura GEEK y puedo ayudarte a crear una pagina como esta :3</h1>
+<br>
+<h2>Hola, soy Sergio Quetzal, estudiante de Ingeniería en Sistemas Computacionales con especialidad en desarrollo web. Me apasiona la creación de interfaces intuitivas, la gestión de bases de datos relacionales y el desarrollo de soluciones tecnológicas innovadoras. Siempre estoy abierto a nuevos aprendizajes y retos.</h2>
+<br>
 <h3>soy estudiante de la carrera de Sistemas computacionales con especialidad en paginas web, me gusta mucho inovar y trabajar con nuevas tecnologias, adaptable a los cambios y a los entornos</h3>
